f757af029345b9a3b7ed021fd8f32895d3daffb9
yunsangee <michael0622@naver.com>
2024-06-29 19:41:43 +0900

    'topbar'

--- src/HikingRecord/HikingRecord.js
+++ src/HikingRecord/HikingRecord.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Cookies from 'js-cookie';
 import Swal from 'sweetalert2';
+import '../App.css';
 
 const styles = {
   pageHeader: {
